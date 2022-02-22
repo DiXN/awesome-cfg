2c3eebd39239cdde16aa3e067daadb90402681a3
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2022-02-22 02:44:01 +0100

    Check for GPU acceleration.

--- scripts/compositor.sh
+++ scripts/compositor.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+shopt -s nocasematch
+
 # Terminate already running bar instances
 killall -q picom
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 
-picom --experimental-backends --config $HOME/.config/picom/conf
+ACCELERATED=$(glxinfo -B | awk '/Accelerated:/{ print $2 }')
+
+if [ "$ACCELERATED" = "yes" ]; then
+  picom --experimental-backends --config "$HOME/.config/picom/conf"
+ else
+  picom --experimental-backends --backend xrender --config "$HOME/.config/picom/conf"
+fi
+
