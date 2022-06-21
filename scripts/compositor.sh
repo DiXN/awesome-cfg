bfc01b53748a34ecba5e7b98c906cb39747b2dc5
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2022-06-22 01:07:58 +0200

    Fix compositor detection script.

--- scripts/compositor.sh
+++ scripts/compositor.sh
@@ -8,9 +8,11 @@ killall -q picom
 # Wait until the processes have been shut down
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 
-ACCELERATED=$(glxinfo -B | awk '/Accelerated:/{ print $2 }')
+GLX_INFO=$(glxinfo -B)
 
-if [ "$ACCELERATED" = "yes" ]; then
+ACCELERATED=$(echo "$GLX_INFO" | awk '/Accelerated:/{ print $2 }')
+
+if [ "$ACCELERATED" = "yes" ] || echo "$GLX_INFO" | grep -q 'OpenGL vendor string: NVIDIA Corporation'; then
   picom --experimental-backends --config "$HOME/.config/picom/conf"
  else
   picom --experimental-backends --backend xrender --config "$HOME/.config/picom/conf"
